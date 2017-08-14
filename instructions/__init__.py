698ba98a9653ab8e36a202208787b916acb2b92a
LeonardoSun <LeonardoSun@users.noreply.github.com>
2017-08-14 10:20:42 +0800

    init

---
+++ instructions/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+    pyasm.instructions.__init__
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: 2008 by Florian Boesch <pyalot@gmail.com>.
+    :license: GNU AGPL v3 or later, see LICENSE for more details.
+"""
+
+from pyasm.instructions.basic import bytes
+from pyasm.instructions.string import mov, cmova, cmovb, lea
+from pyasm.instructions.stack import push, pop, pushfd, popfd
+from pyasm.instructions.math import add, sub, inc, dec, mul
+from pyasm.instructions.control import (
+    ret, call, interrupt,
+    jmp,
+    je, jne, ja, jl, jle, jb, jbe, ja, jae, jg,
+    nop,
+)
+from pyasm.instructions.conditional import cmp
+from pyasm.instructions.fpu import (
+    fmul, fmulp, fdiv, fdivp, fdivr,
+    fsub, fsubr, fadd, faddp,
+    fld, fst, fstp, fdecstp,
+    fcomi,
+    fsqrt,
+    fcmovb,
+)
